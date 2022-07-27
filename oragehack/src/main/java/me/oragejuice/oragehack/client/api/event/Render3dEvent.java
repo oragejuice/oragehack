533f0d7a62c3e4105f103a526f57cb7ab1b2fd5e
jennifer-uwu <77944228+jennifer-uwu@users.noreply.github.com>
2022-07-27 18:48:06 -0400

    scuffed outlineutils esp and render3d event

---
+++ oragehack/src/main/java/me/oragejuice/oragehack/client/api/event/Render3dEvent.java
@@ -0,0 +1,10 @@
+package me.oragejuice.oragehack.client.api.event;
+
+public class Render3dEvent {
+
+    public final float partialTicks;
+
+    public Render3dEvent(float partialTicks) {
+        this.partialTicks = partialTicks;
+    }
+}
